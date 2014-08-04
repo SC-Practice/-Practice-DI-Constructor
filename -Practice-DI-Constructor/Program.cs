0d97e3c97f6e15737080e4ed90dc4eed0117eb90
SeanChen <livebreeze@gmail.com>
2014-08-04 17:20:19 +0800

    未提煉介面版本的 Authentication (noDI)

--- -Practice-DI-Constructor/Program.cs
+++ -Practice-DI-Constructor/Program.cs
@@ -10,6 +10,30 @@ namespace _Practice_DI_Constructor
     {
         static void Main(string[] args)
         {
+            Login("", "", messageSendType: "簡訊"); //messageSendType: 1.Email 2.簡訊
+
+            Console.Read();
         }
+
+        public static void Login(string userId, string password, string messageSendType)
+        {
+            var authService = new AuthenticationService();                      
+
+            #region 使用 authService 物件進行驗證判斷
+            if (authService.TwoFactorLogin(userId, password))
+            {
+                if (authService.VerifyToken("123456"))
+                {
+                    // 登入成功。
+                    Console.WriteLine("登入成功");
+                    return;
+
+                }
+            }
+
+            // 登入失敗。
+            Console.WriteLine("登入失敗");
+            #endregion            
+        }    
     }
 }
